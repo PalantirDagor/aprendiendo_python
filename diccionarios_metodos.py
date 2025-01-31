e3e2cf89434f1e2de0c01bfb6d5dfbc40183095b
PalantirDagor <duber.alexander@gmail.com>
2025-01-30 21:37:42 -0500

    set, dict, fun global e if, clase 5

--- diccionarios_metodos.py
+++ diccionarios_metodos.py
@@ -1,3 +1,5 @@
+import pprint
+
 """
     Diccionarios en Python y sus metodos y usos
 """
@@ -63,3 +65,95 @@ print("antes",copia_a )
 copia_a.update(a_concatenar)
 print("después",copia_a )
 
+"""
+    Forma de concatenar utilizando el truco de los ** o *
+    KEY ARGS | KARGS: **
+    ARGS: *
+"""
+dic_concat_kargt: dict = {**copia_b, **a_concatenar}
+print(dic_concat_kargt)
+
+"""
+    Metodo PopItem: Nos devuelve el ultimo item que elimina
+"""
+
+llave, valor = dic_concat_kargt.popitem()
+print("llave eliminada ", llave, "valor_eliminado de la llave", valor)
+print(dic_concat_kargt)
+
+"""
+    Metodo Clear: Nos elimina todos los items del diccionario
+"""
+print("Antes: ", dic_concat_kargt)
+dic_concat_kargt.clear()
+print("Despues: ",dic_concat_kargt)
+
+"""
+    Uso y manejo de los niveles de profundidad dentro de un diccionario
+
+cedula
+  datos personales:
+    nombres
+    apellidos
+    estado civil
+    fecha nacimiento
+    genero
+    tipo sangre
+      rh
+      grupo_sanguineo
+
+  datos de contacto
+    telefono
+    email
+    fax
+    celuar
+    fijo
+    direcciones
+      [
+        direccion
+        estado:
+      ]      
+  
+"""
+dict_datos_per: dict = {}
+dict_datos_per = {
+    "nombre": "pepito",
+    "apellidos": "perez",
+    "estado_civil": "solteros",
+    "fecha_nacimiento": "01/01/2000",
+    "genero": "masculino",
+    "tipo_sangre": {
+      "rh": "+",
+      "grupo_sanguineo": "AB"
+     }
+}
+
+pprint.pprint(dict_datos_per)
+
+datos_contacto = {
+    "telefono": "4444444",
+    "email": "demo@demo.com",
+    "fax": None,
+    "celular": "3000000000",
+    "fijo": None,
+    "direcciones": (
+        {
+            "direccion": "parque del poblado",
+            "estado": "activa"
+        },
+        {
+            "direccion": "el parque del periodista",
+            "estado": "inactiva"
+        },
+    )
+}
+
+dict_datos_per["datos de contacto"] = datos_contacto
+
+pprint.pprint(dict_datos_per)
+
+print(dict_datos_per["datos de contacto"]["direcciones"][1]["direccion"])
+
+dict_cedula: dict = {}
+dict_cedula["1128456977"] = dict_datos_per
+pprint.pprint(dict_cedula)
